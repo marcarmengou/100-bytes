2c699dffd6e7c670627ed5e839846b7a5f3b6c81
Marc Armengou <83702259+MarcArmengou@users.noreply.github.com>
2023-05-31 09:31:49 +0200

    Add files via upload

--- functions.php
+++ functions.php
@@ -1,21 +1,22 @@
 <?php
 if (!function_exists('bytes100_setup')) :
-function bytes100_setup() {
-  load_theme_textdomain('100-bytes');
-  add_theme_support('automatic-feed-links');
-  add_theme_support('title-tag');
-  add_theme_support('post-thumbnails');
-  register_nav_menus(array('primary' => __('Primary Menu', '100-bytes')));
-  add_theme_support('html5', array('search-form', 'comment-form', 'comment-list', 'gallery', 'caption'));
-  add_theme_support('customize-selective-refresh-widgets');
-  add_theme_support('responsive-embeds');
+  function bytes100_setup() {
+      load_theme_textdomain('100-bytes');
+      add_theme_support('automatic-feed-links');
+      add_theme_support('title-tag');
+      add_theme_support('post-thumbnails');
+      register_nav_menus(array('primary' => __('Primary Menu', '100-bytes')));
+      add_theme_support('html5', array('search-form', 'comment-form', 'comment-list', 'gallery', 'caption'));
+      add_theme_support('customize-selective-refresh-widgets');
+      add_theme_support('responsive-embeds');
 }
 endif;
 add_action('after_setup_theme', 'bytes100_setup');
 
 // Enqueue style.css
 function theme_enqueue_styles() {
-    wp_enqueue_style( 'main-css', get_stylesheet_uri() );
+  wp_enqueue_style( 'main-css', get_stylesheet_uri() );
 }
+
 add_action( 'wp_enqueue_scripts', 'theme_enqueue_styles' );
 ?>
--- readme.txt
+++ readme.txt
@@ -3,8 +3,8 @@ Contributors: Marc Armengou
 Tags: blog, one-column, full-width-template
 Requires at least: 5.7
 Tested up to: 6.2
-Requires PHP: 7.4
-Stable tag: 1.0.4
+Requires PHP: 5.6
+Stable tag: 1.0.6
 License: GNU General Public License v2 or later
 License URI: http://www.gnu.org/licenses/gpl-2.0.html
 
@@ -23,6 +23,14 @@ This is a minimalist WordPress theme that uses just 100 bytes of CSS for its des
 100 Bytes is distributed under the terms of the GNU GPL
 
 == Changelog ==
+
+= 1.0.6 =
+
+* Some corrections
+
+= 1.0.5 =
+
+* Some corrections
 
 = 1.0.4 =
 
