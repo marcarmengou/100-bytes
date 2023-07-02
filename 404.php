be56e3b59ccec1fb3dc764608a2ad3f3bd4b1d42
Marc Armengou <83702259+MarcArmengou@users.noreply.github.com>
2023-07-02 12:31:57 +0200

    1.1.0 version

---
+++ 404.php
@@ -0,0 +1,16 @@
+<?php get_header(); ?>
+
+<div>
+    <h1>
+        <?php 
+        esc_html_e( 'Oops! That page can&rsquo;t be found.', '100-bytes' ); 
+        ?>
+    </h1>
+    <p>
+        <?php 
+        esc_html_e( 'It looks like nothing was found at this location. Maybe try a search?', '100-bytes' ); 
+        ?>
+    </p>
+</div>
+
+<?php get_footer(); ?>
